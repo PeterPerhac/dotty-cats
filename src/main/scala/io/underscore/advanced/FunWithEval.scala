4d3b60b7c4e1336ae3db09c3b4adfd3f4138ac1e
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-02-13 13:08:17 +0100

    stack safe foldright exercise with Eval monad

--- src/main/scala/io/underscore/advanced/FunWithEval.scala
+++ src/main/scala/io/underscore/advanced/FunWithEval.scala
@@ -7,9 +7,18 @@ object FunWithEval {
 
   def main(args: Array[String]): Unit = {
     val ans = for {
-      a <- Eval.now { println("Calculating A"); 35}
-      b <- Eval.always { println("Calculating B"); 5}
-      c <- Eval.later { println("Calculating C"); 2}
+      a <- Eval.now {
+        println("Calculating A");
+        35
+      }
+      b <- Eval.always {
+        println("Calculating B");
+        5
+      }
+      c <- Eval.later {
+        println("Calculating C");
+        2
+      }
     } yield {
       println("Adding A and B and C")
       a + b + c
@@ -20,7 +29,13 @@ object FunWithEval {
 
     def factorial(n: BigInt): Eval[BigInt] = if (n == 1) Eval.now(n) else Eval.defer(factorial(n - 1).map(_ * n))
 
-    println(factorial(50000).value)
+    println(factorial(500).value)
+
+    def foldRight[A, B](as: List[A], acc: B)(f: (A, B) => B): Eval[B] = as match {
+      case h :: t => Eval.defer(foldRight(t, acc)(f)).map(f(h, _))
+      case Nil => Eval.now(acc)
+    }
+    println(foldRight((1 to 10000).toList, "")((i, acc) => acc + i.toString).value)
   }
 
 }

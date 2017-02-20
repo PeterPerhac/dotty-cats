6764f7d3e7161bbd15764d76b1e872ea6fdf188e
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-02-20 12:57:21 +0100

    state monad exercise

---
+++ src/main/scala/io/underscore/advanced/StateMonadPostOrderCalculator.scala
@@ -0,0 +1,44 @@
+package io.underscore.advanced
+
+import cats.data.State
+
+object StateMonadPostOrderCalculator {
+
+  type CalcState[A] = State[List[Double], A]
+
+  def operator(op: (Double, Double) => Double): CalcState[Double] =
+    State[List[Double], Double] {
+      case d1 :: d2 :: tail =>
+        val ret = op(d1, d2)
+        (ret :: tail, ret)
+      case _ => sys.error("invalid operand stack")
+    }
+
+
+  def operand(d: Double): CalcState[Double] =
+    State[List[Double], Double] {
+      stack => (d :: stack, d)
+    }
+
+
+  def evalOne(sym: String): CalcState[Double] = sym match {
+    case "+" => operator(_ + _)
+    case "-" => operator(_ - _)
+    case "*" => operator(_ * _)
+    case "/" => operator(_ / _)
+    case n => operand(n.toDouble)
+  }
+
+  def evalAll(input: List[String]): CalcState[Double] = {
+    import cats.syntax.applicative._
+    input.foldLeft(0d.pure[CalcState]) { (a, b) =>
+      a flatMap (_ => evalOne(b))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val program = evalAll(List("1", "2", "+", "3", "*"))
+    println(program.runA(Nil).value)
+  }
+
+}

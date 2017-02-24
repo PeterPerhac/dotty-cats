ebd6f8d0941251e6c6dd4b2e10fdb36a96b8ad4e
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-02-24 23:45:55 +0000

    parallel vs serial execution of futures with cats' traverse/sequence/cartesian vs monad

---
+++ src/main/scala/parallelFutures/ParallelVsSerialExecution.scala
@@ -0,0 +1,63 @@
+package parallelFutures
+
+import cats.Traverse
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration.Inf
+import scala.concurrent.{Await, Future}
+
+object ParallelVsSerialExecution {
+
+  private def printer: (List[String]) => Unit = _.foreach(println)
+
+  private def tuplePrinter: (String, String, String) => Unit = {
+    case res@_ => res.productIterator.foreach(println)
+  }
+
+  def findFruit(fruit: String) = Future {
+    println(s"${System.currentTimeMillis()} looking for yummy fruit")
+    Thread.sleep(1000)
+    s"I found your $fruit"
+  }
+
+  def sequentialFuturesWithMonads(): Future[Unit] = {
+    for {
+      a <- findFruit("apple")
+      b <- findFruit("banana")
+      c <- findFruit("cherry")
+    } yield {
+      Seq(a, b, c).foreach(println)
+    }
+  }
+
+  def parallelFuturesWithTraverse(): Future[Unit] = {
+    import cats.instances.future._
+    import cats.instances.list._
+    import cats.syntax.traverse._
+    List("apple", "banana", "cherry") traverse findFruit map printer
+  }
+
+  def parallelFuturesWithSequence(): Future[Unit] = {
+    import cats.instances.future._
+    import cats.instances.list._
+    Traverse[List] sequence List(findFruit("apple"), findFruit("banana"), findFruit("cherry")) map printer
+  }
+
+
+  def parallelFuturesWithCartesians(): Future[Unit] = {
+    import cats.instances.future._
+    import cats.syntax.cartesian._
+    findFruit("apple") |@| findFruit("banana") |@| findFruit("cherry") map tuplePrinter
+  }
+
+  def main(args: Array[String]): Unit = {
+    Await.result(sequentialFuturesWithMonads(), Inf)
+    println("\n===\n")
+    Await.result(parallelFuturesWithTraverse(), Inf)
+    println("\n===\n")
+    Await.result(parallelFuturesWithSequence(), Inf)
+    println("\n===\n")
+    Await.result(parallelFuturesWithCartesians(), Inf)
+  }
+
+}

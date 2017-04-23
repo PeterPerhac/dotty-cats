b6204934d6bec4a162c2237978557ffbd13a461e
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-04-23 18:11:51 +0100

    copy-pasted scan-line flood fill algorithm and made it count the stars

--- src/main/scala/exercises/StarCounting.scala
+++ src/main/scala/exercises/StarCounting.scala
@@ -2,17 +2,67 @@ package exercises
 
 import cats.Show
 
+import scala.collection.mutable
+
+
+sealed trait Pixel
+
+case object Darkness extends Pixel
+
+final case class Star(var id: Option[Int] = None) extends Pixel
+
+final case class Row(pixels: Seq[Pixel])
+
+object Row {
+  def fromString(s: String) = Row(s map {
+    case '#' => Darkness
+    case '-' => Star()
+    case _ => throw new RuntimeException("UFO!")
+  })
+
+}
+
+final case class StarWithLocation(star: Star, row: Int, col: Int)
+
+
+object NightSky {
+
+  implicit val starShow = Show.show {
+    (ns: NightSky) =>
+      ns.rows.map(_.pixels.map {
+        case Darkness => "#"
+        case Star(id) => id.fold("-")(_.toString)
+      }.mkString).mkString("\n")
+  }
+
+}
+
+final case class NightSky(w: Int, h: Int, rows: Seq[Row]) {
+
+  val allStars: Seq[StarWithLocation] =
+    for {
+      (row, ridx) <- rows.zipWithIndex
+      (star, cidx) <- row.pixels.zipWithIndex if star.isInstanceOf[Star]
+    } yield {
+      StarWithLocation(star.asInstanceOf[Star], ridx, cidx)
+    }
+
+  def starAt(x: Int, y: Int): Option[Star] = rows(y).pixels(x) match {
+    case Darkness => None
+    case s@Star(id) => Some(s)
+  }
+
+  def identifyStar(x: Int, y: Int, id: Int): Unit = starAt(x, y).foreach(_.id = Some(id))
+
+  override def toString: String = Show[NightSky] show this
+
+}
+
 
 object StarCounting {
 
   type StarCount = Int
 
-  implicit object StarShow extends Show[NightSky] {
-    override def show(f: NightSky): String = f.rows.map(_.pixels.map {
-      case Darkness => "#"
-      case Star(_) => "-"
-    }.mkString).mkString("\n")
-  }
 
   def main(args: Array[String]): Unit = {
     args.headOption.foreach {
@@ -37,51 +87,58 @@ object StarCounting {
     readNightSky(lines, Seq.empty)
   }
 
-  val solver: NightSky => StarCount = nightSky => {
-    println(Show[NightSky] show nightSky)
-    println("...........................")
-    def neighbouringStars(row: Int, col: Int): List[Star] = List(
-      if (row > 0) Some(nightSky.rows(row - 1).pixels(col)) else None, //north
-      if (col > 0) Some(nightSky.rows(row).pixels(col - 1)) else None, //west
-      if (col < nightSky.w - 1) Some(nightSky.rows(row).pixels(col + 1)) else None, //east
-      if (row < nightSky.h - 1) Some(nightSky.rows(row + 1).pixels(col)) else None //south
-    ).flatten.collect {
-      case s: Star => s
+  def floodFill(ns: NightSky, x: Int, y: Int, id: Int): Int = {
+
+    if (ns.starAt(x, y).flatMap(_.id).fold(false)(_ == id)) return id + 1
+
+    val oldId = ns.starAt(x, y).flatMap(_.id)
+    val pixels = new mutable.Stack[(Int, Int)]
+
+    def paint(fx: Int, fy: Int) = ns.identifyStar(fx, fy, id)
+
+    def old(cx: Int, cy: Int): Boolean = ns.starAt(cx, cy).fold(false)(_.id == oldId)
+
+    def push(px: Int, py: Int) = pixels.push((px, py))
+
+    // starting point
+    push(x, y)
+
+    while (pixels.nonEmpty) {
+      val (x, y) = pixels.pop()
+      var y1 = y
+      while (y1 >= 0 && old(x, y1)) y1 -= 1
+      y1 += 1
+      var spanLeft = false
+      var spanRight = false
+      while (y1 < ns.h && old(x, y1)) {
+        paint(x, y1)
+        if (x > 0 && spanLeft != old(x - 1, y1)) {
+          if (old(x - 1, y1)) push(x - 1, y1)
+          spanLeft = !spanLeft
+        }
+        if (x < ns.w - 1 && spanRight != old(x + 1, y1)) {
+          if (old(x + 1, y1)) push(x + 1, y1)
+          spanRight = !spanRight
+        }
+        y1 += 1
+      }
     }
 
-    var newId = 0
-    val stars = for {
-      rowWithIndex <- nightSky.rows.zipWithIndex
-      pixelWithIndex <- rowWithIndex._1.pixels.zipWithIndex if pixelWithIndex._1.isInstanceOf[Star]
-    } yield {
-      val (row, col) = rowWithIndex._2 -> pixelWithIndex._2
-      val s = pixelWithIndex._1.asInstanceOf[Star]
-      val stars = neighbouringStars(row, col) :+ s
-      val topId = stars.foldLeft(newId + 1)((b, star) => star.id.fold(b)(identity))
-      stars.foreach(_.id = Some(topId))
-      newId = topId
-      s
+    id + 1
+  }
+
+  val solver: NightSky => StarCount = nightSky => {
+
+    val unidentifiedStar: StarWithLocation => Boolean = _.star.id.isEmpty
+    var newId = 1
+
+    while (nightSky.allStars.exists(unidentifiedStar)) {
+      nightSky.allStars.find(unidentifiedStar).foreach(swl =>
+        newId = floodFill(nightSky, swl.col, swl.row, newId))
     }
-    stars.flatMap(_.id).fold(0)(Math.max)
+    println(Show[NightSky] show nightSky)
+    println("...........................")
+    nightSky.allStars.flatMap(_.star.id).distinct.size
   }
 
 }
-
-sealed trait Pixel
-
-case object Darkness extends Pixel
-
-final case class Star(var id: Option[Int] = None) extends Pixel
-
-final case class Row(pixels: Seq[Pixel])
-
-object Row {
-  def fromString(s: String) = Row(s map {
-    case '#' => Darkness
-    case '-' => Star()
-    case _ => throw new RuntimeException("UFO!")
-  })
-
-}
-
-final case class NightSky(w: Int, h: Int, rows: Seq[Row])

d2b6cb2259d93a990fe59c227f9f73e3969b4b2b
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-05-27 21:08:12 +0100

    match the blog article

--- src/main/scala/parallelFutures/ParallelVsSerialExecution.scala
+++ src/main/scala/parallelFutures/ParallelVsSerialExecution.scala
@@ -1,12 +1,12 @@
 package parallelFutures
 
-import cats.Applicative
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration.Inf
 import scala.concurrent.{Await, Future}
 
 object ParallelVsSerialExecution {
+
+  import cats.instances.future._
 
   def findFruit(fruit: String) = Future {
     println(s"${System.currentTimeMillis()} looking for fruit")
@@ -34,34 +34,33 @@ object ParallelVsSerialExecution {
     } yield Seq(a, b, c).foreach(println)
   }
 
-  def parallelFuturesWithTraverse()(implicit f: Applicative[Future]): Future[Unit] = {
-    import cats.instances.list._
-    import cats.syntax.traverse._
-    List("apple", "banana", "cherry") traverse findFruit map printer
-  }
-
-  def parallelFuturesWithSequence()(implicit f: Applicative[Future]): Future[Unit] = {
+  def parallelFuturesWithSequence(): Future[Unit] = {
     import cats.instances.list._
     import cats.syntax.traverse._
     List(findFruit("apple"), findFruit("banana"), findFruit("cherry")).sequence map printer
   }
 
-  def parallelFuturesWithCartesians()(implicit f: Applicative[Future]): Future[Unit] = {
+  def parallelFuturesWithTraverse(): Future[Unit] = {
+    import cats.instances.list._
+    import cats.syntax.traverse._
+    List("apple", "banana", "cherry") traverse findFruit map printer
+  }
+
+  def parallelFuturesWithCartesians(): Future[Unit] = {
     import cats.syntax.cartesian._
-    findFruit("apple") |@| findFruit("banana") |@| findFruit("cherry") map tuplePrinter
+    findFruit("apple") |@| findFruit("banana") |@| findFruit("cherry") map print3
   }
 
   def main(args: Array[String]): Unit = {
-    import cats.instances.future._
     import cats.syntax.applicative._
     val program = for {
       _ <- sequentialFuturesWithMonads()
       _ <- println("\n===\n").pure
       _ <- parallelFuturesBasic()
       _ <- println("\n===\n").pure
-      _ <- parallelFuturesWithTraverse()
-      _ <- println("\n===\n").pure
       _ <- parallelFuturesWithSequence()
+      _ <- println("\n===\n").pure
+      _ <- parallelFuturesWithTraverse()
       _ <- println("\n===\n").pure
       _ <- parallelFuturesWithCartesians()
     } yield println("\n===\n")
@@ -71,9 +70,6 @@ object ParallelVsSerialExecution {
 
   private def printer: (List[String]) => Unit = _.foreach(println)
 
-  private def tuplePrinter: (String, String, String) => Unit = {
-    case res@_ => res.productIterator.foreach(println)
-  }
+  private def print3: (String, String, String) => Unit = Seq(_, _, _).foreach(println)
 
 }
-

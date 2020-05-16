d9b6c454409e84c1359279c5a7fcfef3d01f9b1e
Peter Perhac <PeterPerhac@users.noreply.github.com>
2020-05-16 21:13:18 +0100

    more keyboard toggles and bells and whistles

--- src/main/scala/gui/DrawingApp.scala
+++ src/main/scala/gui/DrawingApp.scala
@@ -10,22 +10,37 @@ import scala.util.Random
 
 object DrawingApp extends SimpleSwingApplication {
 
-  val W = 600
-  val H = 600
-  val r = 298
+  val W = 1024
+  val H = 1024
+  val r = 500
+
+  private var polyPointCount: Int = 5
+  private var polygon: Array[Point] = newPolygon()
 
   val canvas = new BufferedImage(W, H, BufferedImage.TYPE_INT_RGB)
 
   class DataPanel(frame: MainFrame) extends Panel {
 
-    def myRefresh(): Unit = {
-      val g = canvas.createGraphics()
-      g.setBackground(Color.BLACK)
-      g.setColor(Color.BLACK)
-      g.fill(new Rectangle2D.Float(0f, 0f, W.toFloat, H.toFloat))
-      g.dispose()
+    private var doBlackout: Boolean = true
+    private var distance: Double = 0.66
+    private var pixelCount: Int = 20000
+    private var restrictPointChoice: Boolean = false
+
+    def updateTitle(): Unit =
+      frame.title = s"$polyPointCount-sided polygon. " +
+        f"distance = $distance%.2f, " +
+        s"points = $pixelCount ${if (restrictPointChoice) ", restricted choice of points" else ""}"
+
+    def doRefresh(): Unit = {
+      if (doBlackout) {
+        val g = canvas.createGraphics()
+        g.setBackground(Color.BLACK)
+        g.setColor(Color.BLACK)
+        g.fill(new Rectangle2D.Float(0f, 0f, W.toFloat, H.toFloat))
+        g.dispose()
+      }
       plotPoints()
-      frame.title = f"distance = $distance%.2f"
+      updateTitle()
       this.repaint()
     }
 
@@ -35,13 +50,49 @@ object DrawingApp extends SimpleSwingApplication {
 
     reactions += {
       case KeyPressed(_, Key.Up, _, _) =>
-        distance = distance + 0.01d
-        myRefresh()
+        distance = distance + 0.005d
+        doRefresh()
       case KeyPressed(_, Key.Down, _, _) =>
-        distance = distance - 0.01d
-        myRefresh()
+        distance = distance - 0.005d
+        doRefresh()
+      case KeyPressed(_, Key.S, _, _) =>
+        pixelCount = pixelCount + 100
+        doRefresh()
+      case KeyPressed(_, Key.X, _, _) =>
+        pixelCount = pixelCount - 100
+        doRefresh()
+      case KeyPressed(_, Key.Space, _, _) =>
+        doBlackout = !doBlackout
+        doRefresh()
+      case KeyPressed(_, Key.R, _, _) =>
+        restrictPointChoice = !restrictPointChoice
+        doRefresh()
+      case KeyPressed(_, Key.A, _, _) =>
+        polyPointCount = polyPointCount + 1
+        polygon = newPolygon()
+        if (doBlackout) doRefresh() else updateTitle()
+      case KeyPressed(_, Key.Z, _, _) =>
+        if (polyPointCount > 3) {
+          polyPointCount = polyPointCount - 1
+          polygon = newPolygon()
+          if (doBlackout) doRefresh() else updateTitle()
+        }
     }
 
+    val randomPoint: Int => (Point, Int) =
+      prevIdx => {
+        if (restrictPointChoice) {
+          val rIdx = LazyList
+            .unfold(prevIdx) { last =>
+              val res = Random.nextInt(polyPointCount)
+              PartialFunction.condOpt(res) { case n if last == prevIdx => n -> n }
+            }
+            .last
+          polygon(rIdx) -> rIdx
+        } else {
+          (polygon(prevIdx), Random.nextInt(polygon.length))
+        }
+      }
     def plotPoints(): Unit =
       LazyList
         .unfold((polygon(0), 0)) {
@@ -52,8 +103,8 @@ object DrawingApp extends SimpleSwingApplication {
         }
         .zipWithIndex
         .map { case (point, i) => point.toPixel(Color.getHSBColor(0.3f, 1.0f, 1.0f)) }
-        .take(50000)
-        .foreach(pixel => canvas.setRGB(pixel.x, pixel.y, pixel.color.getRGB))
+        .slice(1, pixelCount + 1)
+        .foreach(pixel => if (pixel.withinBounds(W, H)) { canvas.setRGB(pixel.x, pixel.y, pixel.color.getRGB) })
 
     override def paintComponent(g: Graphics2D): Unit = {
       plotPoints()
@@ -71,29 +122,31 @@ object DrawingApp extends SimpleSwingApplication {
     def toPixel(c: Color): Pixel = Pixel(this, c)
   }
   case class Pixel(p: Point, color: Color) {
+
+    def withinBounds(maxX: Int, maxY: Int): Boolean =
+      p.x >= 0 && p.x < maxX && p.y >= 0 && p.y < maxY
+
     val x: Int = p.x
     val y: Int = p.y
   }
 
-  val polygon: Array[Point] = Array(
-    Point(300, 10),
-    Point(15, 217),
-    Point(124, 553),
-    Point(476, 553),
-    Point(585, 217)
-  )
+  def polyPoints(sides: Int, cx: Int, cy: Int, r: Int): Array[Point] = {
+    import math._
+    val step: Double = 2 * Pi / sides
+    (0 until sides).toArray.map { idx =>
+      Point((cos(idx * step - Pi / 2) * r).round.toInt, (sin(idx * step - Pi / 2) * r).round.toInt).add(Vector(cx, cy))
+    }
+  }
 
-  val randomPoint: Int => (Point, Int) =
-    n => (polygon(n), Random.nextInt(polygon.length))
+  def newPolygon(): Array[Point] = polyPoints(polyPointCount, W / 2, H / 2, 5 * W / 12)
 
   val partWay: Double => Point => Point => Point =
     d => start => end => start.add(start.to(end).times(d))
 
-  var distance: Double = 2d / 3
-
   override def top: MainFrame = new MainFrame {
     contents = new DataPanel(this) {
       preferredSize = new Dimension(W, H)
+      updateTitle()
     }
   }
 }

059eaaa8abbc9e8b4f5fe00cc352d552ad121bd7
Peter Perhac <PeterPerhac@users.noreply.github.com>
2020-05-19 20:45:42 +0100

    tweaks

--- src/main/scala/gui/Fractals.scala
+++ src/main/scala/gui/Fractals.scala
@@ -8,23 +8,52 @@ import scala.swing._
 import scala.swing.event.{Key, KeyPressed}
 import scala.util.Random
 
-object DrawingApp extends SimpleSwingApplication {
+case class Vector(x: Double, y: Double) {
+  def *(factor: Double): Vector = Vector(x * factor, y * factor)
+}
+case class Point(x: Int, y: Int) {
+  def to(p2: Point): Vector = Vector(p2.x - this.x, p2.y - this.y)
+  def +(v: Vector): Point = Point(this.x + v.x.toInt, this.y + v.y.toInt)
+  def toPixel(c: Color): Pixel = Pixel(this, c)
+}
+case class Pixel(p: Point, color: Color) {
 
-  val W = 1024
-  val H = 1024
-  val r = 500
+  def withinBounds(maxX: Int, maxY: Int): Boolean =
+    p.x >= 0 && p.x < maxX && p.y >= 0 && p.y < maxY
 
-  private var polyPointCount: Int = 5
-  private var polygon: Array[Point] = newPolygon()
+  val x: Int = p.x
+  val y: Int = p.y
+}
 
-  val canvas = new BufferedImage(W, H, BufferedImage.TYPE_INT_RGB)
+object Fractals extends SimpleSwingApplication {
+
+  private val A = 1024
+  private val R = A / 4
 
   class DataPanel(frame: MainFrame) extends Panel {
 
+    val canvas = new BufferedImage(A, A, BufferedImage.TYPE_INT_RGB)
+
+    private var polyPointCount: Int = 3
     private var doBlackout: Boolean = true
-    private var distance: Double = 0.66
-    private var pixelCount: Int = 20000
+    private var distance: Double = 0.50
+    private var pixelCount: Int = 50000
     private var restrictPointChoice: Boolean = false
+
+    private var polygon: Array[Point] = newPolygon(polyPointCount)
+
+    def newPolygon(nSides: Int): Array[Point] = {
+      import math._
+      def int(d: Double): Int = d.round.toInt
+      val step: Double = 2 * Pi / nSides
+      val halfPi = Pi / 2
+      (0 until nSides).toArray.map { idx =>
+        Point(int(cos(idx * step - halfPi) * R), int(sin(idx * step - halfPi) * R)) + Vector(A / 2, A / 2)
+      }
+    }
+
+    val partWay: Double => Point => Point => Point =
+      d => start => end => start + (start.to(end) * d)
 
     def updateTitle(): Unit =
       frame.title = s"$polyPointCount-sided polygon. " +
@@ -36,7 +65,7 @@ object DrawingApp extends SimpleSwingApplication {
         val g = canvas.createGraphics()
         g.setBackground(Color.BLACK)
         g.setColor(Color.BLACK)
-        g.fill(new Rectangle2D.Float(0f, 0f, W.toFloat, H.toFloat))
+        g.fill(new Rectangle2D.Float(0f, 0f, A.toFloat, A.toFloat))
         g.dispose()
       }
       plotPoints()
@@ -63,36 +92,38 @@ object DrawingApp extends SimpleSwingApplication {
         doRefresh()
       case KeyPressed(_, Key.Space, _, _) =>
         doBlackout = !doBlackout
-        doRefresh()
+        if (doBlackout) doRefresh() else updateTitle()
       case KeyPressed(_, Key.R, _, _) =>
         restrictPointChoice = !restrictPointChoice
         doRefresh()
       case KeyPressed(_, Key.A, _, _) =>
         polyPointCount = polyPointCount + 1
-        polygon = newPolygon()
+        polygon = newPolygon(polyPointCount)
         if (doBlackout) doRefresh() else updateTitle()
       case KeyPressed(_, Key.Z, _, _) =>
         if (polyPointCount > 3) {
           polyPointCount = polyPointCount - 1
-          polygon = newPolygon()
+          polygon = newPolygon(polyPointCount)
           if (doBlackout) doRefresh() else updateTitle()
         }
+      case KeyPressed(_, Key.Q, _, _) =>
+        System.exit(0)
     }
 
     val randomPoint: Int => (Point, Int) =
       prevIdx => {
+        def randomVertexIndex(): Int = Random.nextInt(polygon.length)
         if (restrictPointChoice) {
-          val rIdx = LazyList
-            .unfold(prevIdx) { last =>
-              val res = Random.nextInt(polyPointCount)
-              PartialFunction.condOpt(res) { case n if last == prevIdx => n -> n }
-            }
-            .last
-          polygon(rIdx) -> rIdx
+          var newIdx = randomVertexIndex()
+          while (newIdx == prevIdx) {
+            newIdx = randomVertexIndex()
+          }
+          polygon(newIdx) -> newIdx
         } else {
-          (polygon(prevIdx), Random.nextInt(polygon.length))
+          (polygon(prevIdx), randomVertexIndex())
         }
       }
+
     def plotPoints(): Unit =
       LazyList
         .unfold((polygon(0), 0)) {
@@ -101,10 +132,9 @@ object DrawingApp extends SimpleSwingApplication {
             val halfWayPoint = partWay(distance)(p)(nextPoint)
             Some((halfWayPoint, halfWayPoint -> idx))
         }
-        .zipWithIndex
-        .map { case (point, i) => point.toPixel(Color.getHSBColor(0.3f, 1.0f, 1.0f)) }
-        .slice(1, pixelCount + 1)
-        .foreach(pixel => if (pixel.withinBounds(W, H)) { canvas.setRGB(pixel.x, pixel.y, pixel.color.getRGB) })
+        .map(_.toPixel(Color.getHSBColor(0.3f, 1.0f, 1.0f)))
+        .slice(1, pixelCount + 1) //discard annoying first pixel
+        .foreach(pixel => if (pixel.withinBounds(A, A)) { canvas.setRGB(pixel.x, pixel.y, pixel.color.getRGB) })
 
     override def paintComponent(g: Graphics2D): Unit = {
       plotPoints()
@@ -113,39 +143,9 @@ object DrawingApp extends SimpleSwingApplication {
 
   }
 
-  case class Vector(x: Double, y: Double) {
-    def times(factor: Double): Vector = Vector(x * factor, y * factor)
-  }
-  case class Point(x: Int, y: Int) {
-    def to(p2: Point): Vector = Vector(p2.x - this.x, p2.y - this.y)
-    def add(v: Vector): Point = Point(this.x + v.x.toInt, this.y + v.y.toInt)
-    def toPixel(c: Color): Pixel = Pixel(this, c)
-  }
-  case class Pixel(p: Point, color: Color) {
-
-    def withinBounds(maxX: Int, maxY: Int): Boolean =
-      p.x >= 0 && p.x < maxX && p.y >= 0 && p.y < maxY
-
-    val x: Int = p.x
-    val y: Int = p.y
-  }
-
-  def polyPoints(sides: Int, cx: Int, cy: Int, r: Int): Array[Point] = {
-    import math._
-    val step: Double = 2 * Pi / sides
-    (0 until sides).toArray.map { idx =>
-      Point((cos(idx * step - Pi / 2) * r).round.toInt, (sin(idx * step - Pi / 2) * r).round.toInt).add(Vector(cx, cy))
-    }
-  }
-
-  def newPolygon(): Array[Point] = polyPoints(polyPointCount, W / 2, H / 2, 5 * W / 12)
-
-  val partWay: Double => Point => Point => Point =
-    d => start => end => start.add(start.to(end).times(d))
-
   override def top: MainFrame = new MainFrame {
     contents = new DataPanel(this) {
-      preferredSize = new Dimension(W, H)
+      preferredSize = new Dimension(A, A)
       updateTitle()
     }
   }
